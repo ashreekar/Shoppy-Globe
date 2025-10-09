9ad0a628ed758d55774ddb1c971f5dde2a003dc1
ashreekar <ashreekar767@gmail.com>
2025-10-09 12:30:56 +0530

    add export using one index file

---
+++ src/Component/index.js
@@ -0,0 +1,21 @@
+import Cart from "./Cart/Cart";
+import CartItem from "./Cart/CartItem";
+import Footer from "./HeaderandFooter/Footer";
+import Header from "./HeaderandFooter/Header";
+import ProductDetails from "./Products/ProductDetails";
+import ProductItem from "./Products/ProductItem";
+import ProductList from "./Products/ProductList";
+import CheckoutPage from "../Pages/Checkout/CheckoutPage";
+import LandingPage from "../Pages/Landing/LandingPage";
+
+export {
+    Cart,
+    CartItem,
+    Footer,
+    Header,
+    ProductDetails,
+    ProductItem,
+    ProductList,
+    CheckoutPage,
+    LandingPage
+};
