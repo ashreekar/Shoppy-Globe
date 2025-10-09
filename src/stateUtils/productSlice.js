cb526a7e228d3ca5e93dfc14dfa61bc70fd488c1
ashreekar <ashreekar767@gmail.com>
2025-10-09 17:49:22 +0530

    add product slice

---
+++ src/stateUtils/productSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const productSlice=createSlice({
+    name:"Product",
+    initialState:{
+        products:[],
+    },
+    reducers:{
+        addProduct:(state,action)=>{
+            state.products=[]; // making sure state is empty
+
+            action.payload.forEach(ele => {
+                state.products.push({...ele,cartQuantity:0});
+                // already intialising cart quantity helps to render 0 
+                // cart values while fetching from product store
+            });
+        }
+    }
+})
+
+export const {addProduct}=productSlice.actions;
+
+export default productSlice.reducer;
