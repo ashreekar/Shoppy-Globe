73173f142982e3438c058a799a062787be3a26d3
ashreekar <ashreekar767@gmail.com>
2025-10-09 17:18:32 +0530

    add fetch custom hook

---
+++ src/utils/useFetch.js
@@ -0,0 +1,31 @@
+import {useState,useEffect} from "react"
+
+const useFetch=(url)=>{
+    const [data, setdata] = useState(null);
+    const [error, seterror] = useState(null);
+    const [loading,setLoading]=useState(false);
+
+    useEffect(()=>{
+        async function callApi(){
+            try {
+                setLoading(true);
+                const response=await fetch(url);
+                const data=await response.json();
+    
+                if(data){
+                    setdata(data);
+                }
+            } catch (error) {
+                seterror(error);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        callApi();
+    },[url]);
+
+    return {data,error,loading};
+}
+
+export {useFetch};
